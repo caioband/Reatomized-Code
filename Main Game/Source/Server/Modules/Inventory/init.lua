6625211b136f294d6877380ec35d8581f534b1d4
João Vitor Silva Teixeira <123509118+sincevoid@users.noreply.github.com>
2024-01-14 23:33:29 -0300

    23h

---
+++ Main Game/Source/Server/Modules/Inventory/init.lua
@@ -0,0 +1,69 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local InventoryHolder = {}
+
+local ProfileService = require(ReplicatedStorage["[Rojo]"].ProfileService)
+
+local InventoryService = {}
+
+local InventoryData = ProfileService.GetProfileStore("Inventory", {
+	["Players"] = {},
+})
+
+function InventoryService:LoadPlayerInventory(plr: Player, inv: Inventory)
+	local slots = { inv["Slot1"], inv["Slot2"], inv["Slot3"], inv["Slot4"] }
+end
+
+function InventoryService.OnPlayerJoin(player: Player)
+	local joinData = player:GetJoinData()
+	local teleportData = joinData.TeleportData
+
+	local host = teleportData.Host :: number
+	Players:SetAttribute("Host", host)
+
+	local isHost = player.UserId == host
+
+	if isHost == false then
+		return
+	end
+
+	local Profile = InventoryData:LoadProfileAsync(player.UserId, "ForceLoad")
+	if Profile ~= nil then
+		Profile:AddUserId(player.UserId)
+		Profile:Reconcile()
+		Profile:ListenToRelease(function()
+			InventoryHolder[player] = nil
+			player:Kick()
+		end)
+		if player:IsDescendantOf(Players) == true then
+			InventoryHolder[player] = Profile
+			InventoryService.RemoteEvents.Load:FireClient(player, Profile.Data)
+		else
+			Profile:Release()
+		end
+	else
+		player:Kick()
+	end
+end
+
+function InventoryService.OnPlayerLeaving(player)
+	local profile = InventoryHolder[player]
+	if profile then
+		profile:Release()
+		InventoryHolder[player] = nil
+	end
+end
+
+Players.PlayerAdded:Connect(InventoryService.OnPlayerJoin)
+Players.PlayerRemoving:Connect(InventoryService.OnPlayerLeaving)
+
+export type Inventory = {
+	[string]: any,
+
+	["Slot1"]: nil | string,
+	["Slot2"]: nil | string,
+	["Slot3"]: nil | string,
+	["Slot4"]: nil | string,
+}
+return InventoryService
